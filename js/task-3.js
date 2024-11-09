4903a960104fee357e422336ddad32c2294daa63
Viktoriia-Kovalska <kovalskayavi853@gmail.com>
2024-11-09 21:38:01 +0200

    add trim in task 3

--- js/task-3.js
+++ js/task-3.js
@@ -4,7 +4,7 @@ const input = document.querySelector(".input");
 const output = document.querySelector(".output");
 input.addEventListener("input", (event) => {
 output.textContent = event.currentTarget.value;
-if (!event.currentTarget.value || event.currentTarget.value == " ") {
+if (!event.currentTarget.value.trim() || event.currentTarget.value == " ") {
      output.textContent = "Anonymous";
   }
 });
