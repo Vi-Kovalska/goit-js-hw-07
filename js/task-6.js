038c95d3e3b70dc82c681bd4af516da06b6f0890
Viktoriia-Kovalska <kovalskayavi853@gmail.com>
2024-11-11 11:44:04 +0200

    changed cod in task 6 - added div elements to the array before addind them to the DOM so that they must adding in one operation and add the validation type of value entered by the user

--- js/task-6.js
+++ js/task-6.js
@@ -4,34 +4,45 @@ function getRandomHexColor() {
       .toString(16)
       .padStart(6, 0)}`;
   }
-  function createBoxes(amount) {
-    if (amount >= 1 && amount <= 100) {
-        let size = 30;
-        for (let i = 1; i <= amount; i++) {
-        const coloredDiv = document.createElement("div");
-        coloredDiv.style.width = `${size}px`;
-        coloredDiv.style.height = `${size}px`;
-        coloredDiv.style.backgroundColor = getRandomHexColor();
-      boxes.appendChild(coloredDiv);
-        size += 10;
-        }
-      }
-  }
+
   const input = document.querySelector('#amountInput');
-  const btnCreate = document.querySelector('#btnCreate');
-  const btnDestroy = document.querySelector('#btnDestroy');
-  const boxes = document.querySelector('#boxes');
-  
   function create(event) {
     event.preventDefault();
     boxes.innerHTML = "";
     
     const amount = input.value;
     createBoxes(amount);
-    
   }
+
+  const btnCreate = document.querySelector('#btnCreate');
   btnCreate.addEventListener('click', create);
   
+
+  function creaateMarkup() {
+    if (+(amount) < 0 || +(amount) > 100) {
+        alert('The number must be from 1 to 100 inclusive');
+        return;
+        }
+        createBoxes(amount);  
+      }
+      const boxes = document.querySelector('#boxes');
+      let size = 30;
+  function createBoxes(amount) {
+          const arr = [];
+          for (let i = 1; i <= amount; i++) {
+          const coloredDiv = document.createElement("div");
+          coloredDiv.style.width = `${size}px`;
+          coloredDiv.style.height = `${size}px`;
+          coloredDiv.style.backgroundColor = getRandomHexColor();
+          
+          arr.push(coloredDiv);
+        
+          size += 10;
+        }
+        boxes.append(...arr);
+      }
+      
+const btnDestroy = document.querySelector('#btnDestroy');
   const destroyBoxes = () => {
     boxes.innerHTML = '';
     input.value = "";
